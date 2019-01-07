e579abbfb5cdd37e17ac137e4c559b09a6a6bab0
jsp <1552418587@qq.com>
2019-01-07 20:45:40 +0800

    好了

---
+++ src/cn/itcast/test.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("lll");
+        System.out.println("lll");
+        System.out.println("lll");
+        System.out.println("lll");
+        System.out.println("lll");
+    }
+}
